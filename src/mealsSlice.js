817a2182b906f347bb07c9b7be7a540395898575
Joseph McEvoy <joseph_mce@live.co.uk>
2025-11-17 11:35:02 -0500

    Fixed images

--- src/mealsSlice.js
+++ src/mealsSlice.js
@@ -1,13 +1,12 @@
-// mealsSlice.js
 import { createSlice } from '@reduxjs/toolkit';
-
 export const mealsSlice = createSlice({
   name: 'meals',
   initialState: [
-      { name: 'Breakfast', cost: 50, selected: false },
-      { name: 'High Tea', cost: 25, selected: false },
-      { name: 'Lunch', cost: 65, selected: false },
-      { name: 'Dinner', cost: 70, selected: false },
+    { name: 'Breakfast', cost: 50, selected: false },
+{ name: 'High Tea', cost: 25, selected: false },
+{ name: 'Lunch', cost: 65, selected: false },
+{ name: 'Dinner', cost: 70, selected: false },
+   
   ],
   reducers: {
     toggleMealSelection: (state, action) => {
@@ -17,5 +16,4 @@ export const mealsSlice = createSlice({
 });
 
 export const { toggleMealSelection } = mealsSlice.actions;
-
 export default mealsSlice.reducer;
